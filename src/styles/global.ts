f6bd70d40a4a3ffbf9ecfe1dc1090464a498e32f
Cesar Fernandes <cesar.fa@gmail.com>
2020-09-13 10:05:18 -0300

    font installation

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,41 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+  /* fontes inseridas localmente, para fornecer a fonte tbm ao PWA que tem que rodar offline */
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap; /* antes de carregar a fonte principal, ele mostra os textos com a mais próxima do sistema , para nao ficar tudo branco  */
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.woff2') format('woff2') 
+  }
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2') 
+  }
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.woff2') format('woff2') 
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    /* evita o texto cerrilhado em alguns dispositivos, principalmente apple */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
   html {
     font-size: 62.5%;
@@ -13,7 +44,7 @@ const GlobalStyles = createGlobalStyle`
     height: 100%;
   }
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
 
